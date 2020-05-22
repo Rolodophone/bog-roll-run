45e50b40796054a263a039d62d99d6ea2670f8c6
Sergi Lange-Soler <sergi.lange123@gmail.com>
2020-05-22 17:53:48 +0100

    Fixed DuplicatePlatformClasses issue

--- app/src/main/java/net/rolodophone/covidsim/MainActivity.kt
+++ app/src/main/java/net/rolodophone/covidsim/MainActivity.kt
@@ -4,7 +4,6 @@ import net.rolodophone.core.MainActivityCore
 
 class MainActivity : MainActivityCore(R.string.app_name, { ctx -> GameLoadingWindow(ctx) }) {
     override fun onStop() {
-        val x = activeWindow is GameWindow
         activeWindow.let {
             if (it is GameWindow)
                 it.exoplayer.playWhenReady = false
